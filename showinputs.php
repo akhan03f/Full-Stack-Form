eabf698d1add60c1d8c13795d1e742df789832b4
akhan03f <86935890+akhan03f@users.noreply.github.com>
2021-07-04 21:30:28 -0400

    Add files via upload

---
+++ showinputs.php
@@ -0,0 +1,66 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <link rel="stylesheet" type="text/css" href="phpstyle.css">
+  <title>Show songs in the database</title>
+  <meta http-equiv="content-type" content="text/html; charset-iso-8859-1" />
+</head>
+
+<body>
+
+<?php include 'include.htm';?>
+<h1>Show Records</h1>
+
+<?php
+
+  $DBConnect = mysql_connect("itec315.frostburg.edu","akhan03","3125589");
+
+    if ($DBConnect === false)
+      print "Unable to connect to database. Error Number: ".mysql_errno();
+
+    else {
+
+  		$DBname = "akhan03";
+
+  		if (!@mysql_select_db($DBname,$DBConnect))
+  			 print "No database found.";
+
+  		else {
+  			$TableName = "inputs_table";
+  			$SQLString = "select * from $TableName";
+  			$QueryResult = @mysql_query($SQLString, $DBConnect);
+
+  			if (mysql_num_rows($QueryResult) ==0)
+  				print "There are no entries in the database to show";
+
+  			else {
+  				print "Here is the list of inputs in the database<br>";
+  				print "<table width ='100%' border='3px'>";
+  				print "<tr><th>Count (PK)</th><th>First Name</th><th>Last Name</th>
+                <th>Address</th><th>City</th><th>State</th><th>Zip Code</th>
+                <th>Favorite Color</th><th>Favorite Day</th><th>Favorite Number</th></tr>";
+
+  				while (($Row = mysql_fetch_assoc($QueryResult)) !== false) {
+
+  					print"<tr><td>{$Row['Count']}</td>";
+  					print"<td>{$Row['fname']}</td>";
+  					print"<td>{$Row['lname']}</td>";
+  					print"<td>{$Row['address']}</td>";
+            print"<td>{$Row['city']}</td>";
+            print"<td>{$Row['state']}</td>";
+            print"<td>{$Row['zip']}</td>";
+            print"<td>{$Row['color']}</td>";
+            print"<td>{$Row['day']}</td>";
+            print"<td>{$Row['number']}</td></tr>";
+				  }
+
+				print "</table>";
+
+			   }
+			mysql_free_result($QueryResult);
+		  }
+		mysql_close($DBConnect);
+  }
+?>
+
+</body>
+</html>
---
+++ storeinputs.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="phpStyle.css">
+  <title>Stored Inputs</title>
+</head>
+
+<body>
+
+<?php include 'include.htm';?>
+<h1>Store Inputs</h1>
+<h2>The data from the input page has been accepted and stored in the database.</h2>
+
+<?php
+//error_reporting(E_ERROR | E_WARNING | E_PARSE);
+//Hide notices - Notice: Undefined variable: artist in C:\xampp\htdocs\users\akhan03\COSC630\Week 5 Database\storesongs.php on line 34
+
+if(empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['address'])
+ || empty($_POST['city']) || empty($_POST['state']) || empty($_POST['zip'])
+ || empty($_POST['number']) || empty($_POST['color']) || empty($_POST['day']))
+  print "You must enter all of the information.";
+
+else {
+
+  $DBConnect = mysql_connect("itec315.frostburg.edu","akhan03","3125589");
+
+    if ($DBConnect === false)
+      print "Unable to connect to database. Error Number: ".mysql_errno();
+
+    else {
+
+      $DBname = "akhan03";
+      mysql_select_db($DBname,$DBConnect);
+
+      $TableName = "inputs_table";
+      $fname = stripslashes($_POST['fname']);
+      $lname = stripslashes($_POST['lname']);
+      $address = stripslashes($_POST['address']);
+      $city = stripslashes($_POST['city']);
+      $state = stripslashes($_POST['state']);
+      $zip = stripslashes($_POST['zip']);
+      $color = stripslashes($_POST['color']);
+      $day = stripslashes($_POST['day']);
+      $number = stripslashes($_POST['number']);
+
+      $SQLstring = "insert into $TableName(fname, lname, address, city, state, zip, color, day, number)
+       value ('$fname', '$lname', '$address', '$city', '$state', '$zip', '$color', '$day', '$number')";
+      $QueryResult = mysql_query($SQLstring,$DBConnect);
+
+      if ($QueryResult===false)
+        print "There is an error.";
+      else {
+        print "We can assume that the record has been added.";
+      }
+    }
+
+    mysql_close($DBConnect);
+
+  }
+
+?>
+</body>
+</html>
